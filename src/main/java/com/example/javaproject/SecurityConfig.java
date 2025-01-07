3eded2f43d9486113e059faed8af2386c7344db4
kdaon07 <kdaon07@gmail.com>
2025-01-07 15:00:52 +0900

    Logout Fixed 2

--- src/main/java/com/example/javaproject/SecurityConfig.java
+++ src/main/java/com/example/javaproject/SecurityConfig.java
@@ -11,8 +11,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin(origins = "https://jjol.netlify.app")
 @EnableWebSecurity
 public class SecurityConfig {
 
